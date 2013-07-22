318ca1b1a74545010d486a5bfa054dee47ee2812
Oscar M <omarti87@gmail.com>
2013-07-22 01:50:00 +0000

    Fix

--- initkey2.sh
+++ initkey2.sh
@@ -2,5 +2,5 @@
 ssh -T git@github.com
 cd $HOME
 git init
-git config --global user.name"Oscar M"
-git config --global user.email"omarti87@gmail.com"
+git config --global user.name "Oscar M"
+git config --global user.email "omarti87@gmail.com"
